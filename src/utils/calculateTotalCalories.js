72684951c34e76186a1dda538909b39f5b46ba18
Fatih Demir <fd1134@gmail.com>
2025-07-27 22:57:52 +0300

    code done

--- src/utils/calculateTotalCalories.js
+++
@@ -1,8 +0,0 @@
-export const calculateTotalCalories = (weight, kcalPer100g) => {
-  if (typeof weight !== 'number' || typeof kcalPer100g !== 'number') {
-    throw new Error('weight and kcalPer100g should be numerical values.');
-  }
-  if (weight < 0 || kcalPer100g < 0) return 0;
-
-  return (weight * kcalPer100g) / 100;
-};
